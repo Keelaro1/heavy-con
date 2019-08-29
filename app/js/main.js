88920135846d5cb7e0d70a7d473b758af992c25d
Anton Vorobyov <igelness@gmail.com>
2019-08-29 11:59:58 +0300

    ADD slider section

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".slider__inner").slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    fadeSpeed: 5000
+  });
+});
